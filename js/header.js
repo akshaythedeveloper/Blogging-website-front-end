995e25ff138b29a35f0ffbddc98c556fc5b0bc3a
askthedeveloper <akshay.verma0808@gmail.com>
2018-12-08 11:20:55 +0530

    Signin modal implemented

--- js/header.js
+++ js/header.js
@@ -1,3 +1,5 @@
+//Code for Signup modal
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -21,5 +23,33 @@ span.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+};
+
+/* Code for Signin modal */
+
+//Get the modal
+var modaltwo = document.getElementById("myModalSecond");
+
+//Get the button that opens the modal
+var btnSecond = document.getElementById("mySigninBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[1];
+
+// When the user clicks on the button open the modal
+btnSecond.onclick = function() {
+  modaltwo.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modaltwo.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modaltwo) {
+    modaltwo.style.display = "none";
   }
 };
